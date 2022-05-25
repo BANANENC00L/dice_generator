3d47375573190a579ae3829f0d5a62e51d206631
Jonathan <jonathan@Neses-Air.fritz.box>
2022-05-25 16:51:55 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# guess_numbers.py
---
+++ guess_numbers.py
@@ -0,0 +1,21 @@
+﻿import random
+
+v = 0
+a = random.randint(1,100)
+while True:
+    print("Rate meine Zahl!")
+    b = input()
+    b = int(b)
+    if a == b:
+        print("Glückwunsch! Deine Zahl ist richtig!")
+        break
+
+    if b > a:
+        print("Meine Zahl ist kleiner! Du kek!")
+        v = v + 1
+        print("Versuch:",v)
+
+    if b < a:
+        print("Meine Zahl ist größer! Du kek!")
+        v = v + 1
+        print("Versuch:",v)
